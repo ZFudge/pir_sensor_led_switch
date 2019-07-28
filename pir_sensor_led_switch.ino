c267bf3648529f74f3017479d5bf849eaa81273a
zfudge <zacheryfudge@gmail.com>
2019-07-28 15:30:18 -0600

    refactored loop

--- pir_sensor_led_switch.ino
+++ pir_sensor_led_switch.ino
@@ -1,5 +1,4 @@
 
-
 const int LIGHT_PIN = A0; // Pin connected to voltage divider output
 const float VCC = 4.98; // Measured voltage of Ardunio 5V line
 const float R_DIV = 4660.0; // Measured resistance of 3.3k resistor
@@ -10,7 +9,8 @@ const int LED_PIN = 13;
 const int PIR_PIN = 7; // HC-S501 input
 int pirValue;
 
-const int COUNT_START = 150;
+const int COUNT_HIGH = 150;
+const int COUNT_LOW = -7;
 int counter = 0;
 
 const int MS = 500; // loop speed
@@ -30,39 +30,38 @@ void loop() {
   if (lightADC > 0) {
     float lightV = lightADC * VCC / 1023.0;
     float lightResistance = R_DIV * (VCC / lightV - 1.0);
-    Serial.println("lightADC: " + String(lightADC) + " analog");
-    Serial.println("Voltage: " + String(lightV) + " V");
-    Serial.println("Resistance: " + String(lightResistance) + " ohms");
-    Serial.println();
-
+    logPhotocell(lightADC, lightV, lightResistance);
     if (lightResistance >= DARK_THRESHOLD) {
-      if (counter >= 0) {
-        pirValue = digitalRead(PIR_PIN);
-        if (pirValue) {
-          counter = COUNT_START; // 1:45
-        }
-  
-        if (counter < 1) {
-          digitalWrite(LED_PIN, LOW);
-        } else {
-          digitalWrite(LED_PIN, HIGH);
-        }
-      }
+      checkLEDAdjust();
     } else {
-      digitalWrite(LED_PIN, LOW);
-      counter = -5;
+      hardLEDReset();
     }
   } else {
     digitalWrite(LED_PIN, LOW);
   }
 
   countTowardsZero();
-
-  Serial.println("reading: " + String(pirValue));
-  Serial.println("counter: " + String(counter));
-  Serial.println("\n\n");
-
+  logPIR();
   delay(MS);
+}
+
+void checkLEDAdjust() {
+  if (counter >= 0) {
+    pirValue = digitalRead(PIR_PIN);
+    if (pirValue) {
+      counter = COUNT_HIGH; // 1:45
+    }
+    if (counter < 1) {
+      digitalWrite(LED_PIN, LOW);
+    } else {
+      digitalWrite(LED_PIN, HIGH);
+    }
+  }
+}
+
+void hardLEDReset() {
+  digitalWrite(LED_PIN, LOW);
+  counter = COUNT_LOW;
 }
 
 void countTowardsZero() {
@@ -71,4 +70,17 @@ void countTowardsZero() {
   } else if (counter < 0) {
     counter++;
   }
+}
+
+void logPIR() {
+  Serial.println("reading: " + String(pirValue));
+  Serial.println("counter: " + String(counter));
+  Serial.println("\n\n");
+}
+
+void logPhotocell(int adc, float lV, float lR) {
+  Serial.println("lightADC: " + String(adc) + " analog");
+  Serial.println("Voltage: " + String(lV) + " V");
+  Serial.println("Resistance: " + String(lR) + " ohms");
+  Serial.println();
 }
